ee27b25f7a76632a8ef7d17477b3f8c6c1f6f0c8
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-03-29 01:33:03 +0700

    conditions in php

--- www/index.php
+++ www/index.php
@@ -1,19 +1,44 @@
 <?php
 var_dump(!1);
+echo '<br>';
 var_dump(!0);
+echo '<br>';
 var_dump(!true);
+echo '<br>';
 var_dump(2 && 3);
+echo '<br>';
 var_dump(5 && 0);
+echo '<br>';
 var_dump(3 || 0);
+echo '<br>';
 var_dump(5 / 1);
+echo '<br>';
 var_dump(1 / 5);
+echo '<br>';
 var_dump(5 + '5string');
+echo '<br>';
 var_dump('5' == 5);
+echo '<br>';
 var_dump('05' == 5);
+echo '<br>';
 var_dump('05' == '5');
+echo '<br>';
 
 
-
+echo 'Условия в php<br>';
+if ('string') {echo 'Условие выполнилось';}
+echo '<br>';
+if (0) {echo 'Условие выполнилось';}
+echo '<br>';
+if (null) {echo 'Условие выполнилось';}
+echo '<br>';
+if (5) {echo 'Условие выполнилось';}
+echo '<br>';
+if (-5) {echo '-5 Условие выполнилось';}
+echo '<br>';
+//суть в том, что в условии проверяется что-то и это что-то "не пусто" или не является нулём (ключевое слово), то возвращает true
+$chet = 5%2 == 0 ? 'Число чётное': 'Числов нечётное';
+echo $chet;
 ?>
 <!doctype html>
 <html lang="ru">
@@ -30,3 +55,4 @@ var_dump('05' == '5');
 
 </body>
 </html>
+
