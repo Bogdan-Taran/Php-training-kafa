585ff465bf180ed85a502003b18a3040180205b6
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-03-29 18:27:25 +0700

    homework foreach

--- www/index.php
+++ www/index.php
@@ -1,22 +1,18 @@
 <?php
-$array1 = [
-    'element_1' => [
-        'element_1_1' => [
-            'element_1_1_1' => [
-                'element_1_1_1_1',
-                'element_1_1_1_2',
-                'element_1_1_1_3'
-            ],
-            'element_1_1_2' => [
-                'element_1_1_2_1',
-                'element_1_1_2_2',
-            ]
-        ],
-        'element_1_2' => 'value_1_2'
-    ],
-    'element_2' => 'value_2',
-    'element_3' => [],
+$carsSpeeds = [
+    95,
+    140,
+    78
 ];
-$array1['element_1']['element_1_1']['element_1_1_1'] = 'new element';
-print_r($array1);
+
+$sumOfSpeeds = 0;
+$count = 0;
+
+foreach ($carsSpeeds as $speed) {
+    $sumOfSpeeds += $speed;
+    $count++;
+}
+$averageSpeed = $sumOfSpeeds / $count;
+
+echo 'Средняя скорость движения по трассе: ' . $averageSpeed;
 ?>
