0aab76f3d90e39e51638779323920d4f0bbbae10
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-03-29 18:23:28 +0700

    homework arrays

--- www/index.php
+++ www/index.php
@@ -1,17 +1,22 @@
 <?php
-$header = '<h1>HEADER yeah</h1>';
-$header_logo = '<h1>HEADER LOGO</h1>';
-$header_text = '<p>This is header</p>';
-$header_text2 = '<p>And header uses variables</p>';
-
-$sidebar = 'SIDEBAR yeah';
-
-$content = '<h1>Заголовок статьи</h1><p>Текст какой-то статьи</p>';
-
-$footer = '<h2>FOOTER yeah</h2><p>This is footer</p>';
-
-require __DIR__ . '/header.php';
-require __DIR__ . '/sidebar.php';
-require __DIR__ . '/content.php';
-require __DIR__ . '/footer.php';
+$array1 = [
+    'element_1' => [
+        'element_1_1' => [
+            'element_1_1_1' => [
+                'element_1_1_1_1',
+                'element_1_1_1_2',
+                'element_1_1_1_3'
+            ],
+            'element_1_1_2' => [
+                'element_1_1_2_1',
+                'element_1_1_2_2',
+            ]
+        ],
+        'element_1_2' => 'value_1_2'
+    ],
+    'element_2' => 'value_2',
+    'element_3' => [],
+];
+$array1['element_1']['element_1_1']['element_1_1_1'] = 'new element';
+print_r($array1);
 ?>
