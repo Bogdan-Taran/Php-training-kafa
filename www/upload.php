f99c8d5180080ad1368ef68afe32cd1ab3714676
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-03-29 23:25:03 +0700

    create photoalbum in php

--- www/upload.php
+++ www/upload.php
@@ -1,25 +1,17 @@
 <?php
-if (!empty($_FILES['attachment'])) {
+
+require __DIR__ . '/auth.php';
+$login = getUserLogin();
+
+if ($login !== null && !empty($_FILES['attachment'])) {
     $file = $_FILES['attachment'];
 
     $srcFileName = $file['name'];
     $newFilePath = __DIR__ . '/uploads/' . $srcFileName;
-    $sizeFile = $file['size'];
-    $filePath = $file['tmp_name'];
-    $newFilePath = __DIR__ . '/upload/' . $srcFileName;
-    $image = getimagesize($filePath);
-    $limitWidth = 1280;
-    $limitHeight = 720;
 
     $allowedExtensions = ['jpg', 'png', 'gif'];
     $extension = pathinfo($srcFileName, PATHINFO_EXTENSION);
-    if ($sizeFile > 8000000) {
-        $error = 'размер файла слишком большой';
-    }
-    elseif ($file['error'] == UPLOAD_ERR_INI_SIZE) {
-        $error = 'размер файла слишком большой';
-    }
-    else if (!in_array($extension, $allowedExtensions)) {
+    if (!in_array($extension, $allowedExtensions)) {
         $error = 'Загрузка файлов с таким расширением запрещена!';
     } elseif ($file['error'] !== UPLOAD_ERR_OK) {
         $error = 'Ошибка при загрузке файла.';
@@ -28,25 +20,31 @@ if (!empty($_FILES['attachment'])) {
     } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
         $error = 'Ошибка при загрузке файла';
     } else {
-        $result = 'http://myproject.loc/uploads/' . $srcFileName;
+        $result = 'http://myproject.local/www/uploads/' . $srcFileName;
     }
 }
 ?>
-
 <html>
 <head>
     <title>Загрузка файла</title>
 </head>
 <body>
-<?php if (!empty($error)): ?>
-    <?= $error ?>
-<?php elseif (!empty($result)): ?>
-    <?= $result ?>
+<?php if ($login === null): ?>
+    <a href="/www/login.php">Авторизуйтесь</a>
+<?php else: ?>
+    Добро пожаловать, <?= $login ?> |
+    <a href="/www/logout.php">Выйти</a>
+    <br>
+    <?php if (!empty($error)): ?>
+        <?= $error ?>
+    <?php elseif (!empty($result)): ?>
+        <?= $result ?>
+    <?php endif; ?>
+    <br>
+    <form action="/www/upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="attachment">
+        <input type="submit">
+    </form>
 <?php endif; ?>
-<br>
-<form action="/www/upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="attachment">
-    <input type="submit">
-</form>
 </body>
 </html>
